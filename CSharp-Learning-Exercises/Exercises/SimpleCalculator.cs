8f3908647cb41ecdcd672ed766cd237d41a4f264
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-04-30 12:49:39 +0100

    Creating SimpleCalculator class

---
+++ CSharp-Learning-Exercises/Exercises/SimpleCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Learning_Exercises.Exercises
+{
+    internal class SimpleCalculator
+    {
+    }
+}
