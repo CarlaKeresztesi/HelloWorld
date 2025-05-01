411195ac0addf06e270f3d2886da1362a52f1904
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-05-01 23:13:40 +0100

    EvenOddChecker class enabled

--- EvenOddChecker.cs
+++ EvenOddChecker.cs
@@ -5,5 +5,18 @@ namespace HelloWorld
 
 	public class EvenOddChecker
 	{
-		
-	}
+    public static void Run()
+    {
+        Console.WriteLine("Hello! Please enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"{number} is even.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is odd.")
+        }
+    }
+    }
