603201994c59d044050d91c7aff8f7e231a5261b
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-05-02 14:10:59 +0100

    AgeCalculator updated

--- AgeCalculator.cs
+++ AgeCalculator.cs
@@ -4,7 +4,16 @@ namespace HelloWorld
 {
     public class AgeCalculator
     {
-      
+        public static void Run()
+        {
+            Console.WriteLine("Hello! What year were you born?");
+            int birthYear = int.Parse(Console.ReadLine());
+
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - birthYear;
+
+            Console.WriteLine($"You are {age} years old.");
+        }
     }
 }
 
