d5392bfe125f247eccb8047ad77bf8a75e4cb7ea
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-05-03 11:29:16 +0100

    MultiplicationTableGenerator class code added

--- MultiplicationTableGenerator.cs
+++ MultiplicationTableGenerator.cs
@@ -6,7 +6,13 @@ namespace HelloWorld
     {
         public static void Run()
         {
+            Console.WriteLine("Hello! Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
 
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine($"{i} x {num} = {num * i}");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            TemperatureConverter.Run();
+            MultiplicationTableGenerator.Run();
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
