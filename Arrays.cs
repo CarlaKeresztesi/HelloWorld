717830fe069ea05164df97bb746bb8f66700de5e
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-05-05 20:23:47 +0100

    Arrays class - using indexes & a variable, to retrieve data

--- Arrays.cs
+++ Arrays.cs
@@ -13,6 +13,11 @@ namespace HelloWorld
             Console.WriteLine($"arrayOfStrings has {arrayOfStrings.Length} elements");
             Console.WriteLine($"arrayOfInts has {arrayOfInts.Length} elements");
             Console.WriteLine($"arrayOfLetters has {arrayOfLetters.Length} elements");
+
+            Console.WriteLine($"{arrayOfStrings[0]} {arrayOfStrings[1]} {arrayOfStrings[2]} {arrayOfStrings[3]}");
+            Console.WriteLine($"{arrayOfInts[1]} {arrayOfInts[2]} {arrayOfInts[3]} {arrayOfInts[4]} {arrayOfInts[5]}}");
+            Console.WriteLine($"{arrayOfStrings[{arrayOfInts[3]}]}");
+                
         }
     }
 }
