a72719f698be7ff600e7f2e35f7543e3de1d4767
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-05-05 17:44:27 +0100

    IntegerDataTypes class

---
+++ IntegerDataTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class IntegerDataTypes
+    {
+        public static void Run()
+        {
+            byte piccolo = byte.MaxValue;
+            short small = short.MaxValue;
+            int regular = int.MaxValue;
+            long grande = long.MaxValue;
+
+            Console.WriteLine("piccolo")
+        }
+    }
+}
+
