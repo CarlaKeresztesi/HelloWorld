7fdfcf88f78f4a42e21ab644456a978445a2b66d
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-05-05 20:36:17 +0100

    Arrays class - assigning values to arrayOfLetters

--- Arrays.cs
+++ Arrays.cs
@@ -15,9 +15,18 @@ namespace HelloWorld
             Console.WriteLine($"arrayOfLetters has {arrayOfLetters.Length} elements");
 
             Console.WriteLine($"{arrayOfStrings[0]} {arrayOfStrings[1]} {arrayOfStrings[2]} {arrayOfStrings[3]}");
-            Console.WriteLine($"{arrayOfInts[1]} {arrayOfInts[2]} {arrayOfInts[3]} {arrayOfInts[4]} {arrayOfInts[5]}}");
-            Console.WriteLine($"{arrayOfStrings[{arrayOfInts[3]}]}");
-                
+            Console.WriteLine($"{arrayOfInts[1]} {arrayOfInts[2]} {arrayOfInts[3]} {arrayOfInts[4]}");
+            Console.WriteLine($"{arrayOfStrings[arrayOfInts[3]]}");
+
+            string cowColor = arrayOfStrings[arrayOfInts[3]];
+
+            arrayOfLetters = new char[cowColor.Length];
+            arrayOfLetters[0] = 'c';
+            arrayOfLetters[1] = cowColor[1]; //assigning values to arrayOfLetters by accessing the cowColor string variable in the same way we access an array - a string is a special array of char
+            arrayOfLetters[2] = cowColor[2];
+            arrayOfLetters[3] = cowColor[3];
+            arrayOfLetters[4] = cowColor[4];
+
         }
     }
 }
