e15e82430ba0619a51aa6fd7e7816699f131c622
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-05-06 11:14:36 +0100

    Arrays class - added try catch

--- Arrays.cs
+++ Arrays.cs
@@ -7,8 +7,8 @@ namespace HelloWorld
         public static void Run()
         {
             string[] arrayOfStrings = { "how", "now", "brown", "cow" };
-            int[] arrayOfInts = { 0, 0, 1, 2, 3 };
-            char[] arrayOfLetters = {};
+            int[] arrayOfInts = new int[] { 0, 0, 1, 2, 3 };
+            char[] arrayOfLetters = { };
 
             Console.WriteLine($"arrayOfStrings has {arrayOfStrings.Length} elements");
             Console.WriteLine($"arrayOfInts has {arrayOfInts.Length} elements");
@@ -18,17 +18,28 @@ namespace HelloWorld
             Console.WriteLine($"{arrayOfInts[1]} {arrayOfInts[2]} {arrayOfInts[3]} {arrayOfInts[4]}");
             Console.WriteLine($"{arrayOfStrings[arrayOfInts[3]]}");
 
-            string cowColor = arrayOfStrings[arrayOfInts[3]];
+            string cowColor = "arrayOfStrings[arrayOfInts[3]]";
+            Console.WriteLine($"cowColor = {cowColor}");
 
-            arrayOfLetters = new char[cowColor.Length];
-            arrayOfLetters[0] = 'c';
-            arrayOfLetters[1] = cowColor[1]; //assigning values to arrayOfLetters by accessing the cowColor string variable in the same way we access an array - a string is a special array of char
-            arrayOfLetters[2] = cowColor[2];
-            arrayOfLetters[3] = cowColor[3];
-            arrayOfLetters[4] = cowColor[4];
+            try
+            {
+                if (cowColor.Length >= 5)
+                {
+                    arrayOfLetters = new char[cowColor.Length];
 
-            Console.WriteLine($"{arrayOfLetters[0]} {arrayOfLetters[1]} {arrayOfLetters[2]} {arrayOfLetters[3]} {arrayOfLetters[4]}");
+                    arrayOfLetters[0] = 'c';
+                    arrayOfLetters[1] = cowColor[1]; //assigning values to arrayOfLetters by accessing the cowColor string variable in the same way we access an array - a string is a special array of char
+                    arrayOfLetters[2] = cowColor[2];
+                    arrayOfLetters[3] = cowColor[3];
+                    arrayOfLetters[4] = cowColor[4];
 
+                    Console.WriteLine($"{arrayOfLetters[0]} {arrayOfLetters[1]} {arrayOfLetters[2]} {arrayOfLetters[3]} {arrayOfLetters[4]}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured: {ex.Message}");
+            }
         }
     }
 }
