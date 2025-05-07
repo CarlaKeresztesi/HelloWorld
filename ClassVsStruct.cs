f1ac33d8a5be2dac8fbaf3a13c6a9f969e1c476d
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-05-07 22:21:14 +0100

    ClassVsStruct class - added class

--- ClassVsStruct.cs
+++ ClassVsStruct.cs
@@ -7,7 +7,14 @@ namespace HelloWorld
     {
         public static void Run()
         {
-            
+            var classPoint1 = new PointClass { X  = 1, Y = 2 };
+            var classPoint2 = classPoint1;
+
+            classPoint2.X = 99;
+
+            Console.WriteLine("=== Class ===");
+            Console.WriteLine($"Class Point1: X = {classPoint1.X}, Y = {classPoint1.Y}");
+            Console.WriteLine($"Class Point2: X = {classPoint2.X}, Y = {classPoint2.Y}");
         }
     }
 }
